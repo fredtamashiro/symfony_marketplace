7793a14e16a9e4d7ec36d8dc5c19447eb596f07c
Fred Tamashiro <46597932+fredtamashiro@users.noreply.github.com>
2025-01-12 20:33:35 -0300

    Admin/user controller criado, entity criado.

---
+++ src/Controller/Admin/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/admin/login', name: 'app_admin_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError(); // busca erros de autenticacao
+
+        $lastUsername = $authenticationUtils->getLastUsername(); // busca o ultimo usuario utilizado
+
+        return $this->render('admin/login/index.html.twig', [
+            'lastUsername' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+}
